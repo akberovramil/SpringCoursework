28f88ea179a967abf9b77fd1de29c191015ce71a
akberovramil <ramil.akberov96@gmail.com>
2023-02-08 18:00:03 +0300

    First commit

---
+++ src/test/java/akberov/ramil/coursework/Coursework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package akberov.ramil.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
